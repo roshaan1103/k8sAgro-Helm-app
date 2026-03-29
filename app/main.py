404ce3fa0c67a8bb3ec4c98cd0624e65a9013a72
root <root@roshaan.myguest.virtualbox.org>
2026-03-29 18:08:55 +0500

    Adding prometheus and grafana files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from fastapi.responses import Response
 
 app = FastAPI()
 
+# Example metric: count total requests per endpoint
+REQUEST_COUNTER = Counter("requests_total", "Total HTTP requests", ["endpoint"])
+
 @app.get("/")
 def read_root():
-    return {"message": "Version 3, auto sha checking"}
+    REQUEST_COUNTER.labels(endpoint="/").inc()
+    return {"message": "Hello from AIops App!"}
+
+# Prometheus metrics endpoint
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
